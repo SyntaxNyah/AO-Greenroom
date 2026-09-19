fbda46cf9690477ab510ea83127e9c7325f8eeb0
SyntaxNyah <SyntaxNyah@users.noreply.github.com>
2026-09-19 19:29:00 +0000

    deploy: f2af3ad2ea4625069812073e69662122768ba19a

---
+++ assets/rgbdDecode.fragment-D8RX34Ea.js
@@ -0,0 +1,5 @@
+import{w as e}from"./mmdStage-4pfmdnHk.js";import{h as n}from"./helperFunctions-BmkD8UF-.js";import"./index-Czti-6uv.js";const o="rgbdDecodePixelShader",t=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=t);const a=[n];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const c={name:o,shader:t};export{c as rgbdDecodePixelShader};
