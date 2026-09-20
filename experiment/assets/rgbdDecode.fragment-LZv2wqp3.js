a5547470cc82aaca4775603c8789b6075deb5beb
SyntaxNyah <SyntaxNyah@users.noreply.github.com>
2026-09-20 10:55:23 +0000

    deploy: bbd6880118c110ad746becfddc2d24833c582e77

--- experiment/assets/rgbdDecode.fragment-LZv2wqp3.js
+++
@@ -1,5 +0,0 @@
-import{w as e}from"./mmdStage-DOgKdbar.js";import{h as n}from"./helperFunctions-q186splM.js";import"./index-BaPdAJ8g.js";const o="rgbdDecodePixelShader",t=`varying vec2 vUV;uniform sampler2D textureSampler;
-#include<helperFunctions>
-#define CUSTOM_FRAGMENT_DEFINITIONS
-void main(void) 
-{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=t);const a=[n];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const c={name:o,shader:t};export{c as rgbdDecodePixelShader};
